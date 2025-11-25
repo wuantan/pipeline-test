4379b0f4e9bef5b7f66164d3b8ba6aa5b759ff20
wuantan <168593218+wuantan@users.noreply.github.com>
2025-11-25 12:59:03 -0300

    Add files via upload

---
+++ test-pipeline.groovy
@@ -0,0 +1,93 @@
+pipeline {
+    agent any
+
+    environment {
+        PROJECT_NAME = "pipeline-test"
+        SONARQUBE_URL = "http://54.85.101.56:9000"
+        SONARQUBE_TOKEN ="sqa_b6b88e9d74b195f593d37a178668d7e5cc78c86c"
+        TARGET_URL = "http://172.31.15.234:5000" //  ←✔ ip del servidor cuando lo instalas
+    }
+
+    stages {
+
+        stage('Install Python') {
+            steps {
+                sh '''
+                    apt update
+                    apt install -y python3 python3-venv python3-pip
+                '''
+            }
+        }
+
+        stage('Setup Environment') {
+            steps {
+                sh '''
+                    python3 -m venv venv
+                    . venv/bin/activate
+                    pip install --upgrade pip
+                    pip install Flask==2.2.5 Werkzeug==2.2.3 Jinja2==3.1.2 itsdangerous==2.1.2 click==8.1.3
+                '''
+            }
+        }
+
+        stage('Python Security Audit') {
+            steps {
+                sh '''
+                    . venv/bin/activate
+                    pip install pip-audit
+                    mkdir -p dependency-check-report
+                '''
+
+                script {
+                    def exitCode = sh(
+                        script: '. venv/bin/activate && pip-audit -f markdown -o dependency-check-report/pip-audit.md',
+                        returnStatus: true
+                    )
+
+                    if (exitCode != 0) {
+                        echo "pip-audit encontró vulnerabilidades, pero se continúa."
+                    } else {
+                        echo "pip-audit no encontró vulnerabilidades."
+                    }
+                }
+            }
+        }
+
+        stage('SonarQube Analysis') {
+            steps {
+                script {
+                    def scannerHome = tool 'SonarQube Scanner'  //  ←✔ Nombre de la instalacion
+                    withSonarQubeEnv('SonarServer') {
+                        sh """
+                            ${scannerHome}/bin/sonar-scanner \
+                                -Dsonar.projectKey=${PROJECT_NAME} \
+                                -Dsonar.sources=. \
+                                -Dsonar.host.url=${SONARQUBE_URL}
+                        """
+                    }
+                }
+            }
+        }
+
+        stage('Dependency Check') {
+            steps {//  ←✔ Copia y pega la key en api key
+                dependencyCheck(
+                    additionalArguments: "--scan . --format HTML --out dependency-check-report --enableExperimental --enableRetired --nvdApiKey eafa3341-5ae9-46a7-9673-131a55fad55f", 
+                    odcInstallation: 'DependencyCheck'  //  ←✔ Nombre de la instalacion
+                )
+            }
+        }
+    }  //  ←✔ Cierre correcto de `stages`
+
+    post {
+        always {
+            archiveArtifacts artifacts: 'dependency-check-report/**', fingerprint: true
+        }
+        success {
+            echo "Build success"
+        }
+        failure {
+            echo "Build failed"
+        }
+    }
+}
